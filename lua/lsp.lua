f79f2115f23ac274f605f0fb2f6351115f9d373c
Jeffrey Carr <jeffrey.carr98@gmail.com>
2024-11-25 00:02:12 -0500

    i dont remember

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,3 +1,4 @@
+-- Mason setup for LSP management
 require('mason').setup({
     ui = {
         icons = {
@@ -9,7 +10,6 @@ require('mason').setup({
 })
 
 require('mason-lspconfig').setup({
-    -- A list of servers to automatically install if they're not already installed
     ensure_installed = {
         'pylsp',
         'lua_ls',
@@ -22,7 +22,7 @@ require('mason-lspconfig').setup({
     },
 })
 
--- Set different settings for each languages' LSP
+-- Set different settings for each language's LSP
 local lspconfig = require('lspconfig')
 
 -- Customized on_attach function
@@ -57,6 +57,8 @@ local on_attach = function(client, bufnr)
             end,
         })
     end, bufopts)
+    -- Show signature hints
+    vim.keymap.set("n", "<C-Space", vim.lsp.buf.signature_help, { desc="Signature hint"})
 
     -- gopls settings
     if client.name == 'gopls' then
@@ -110,3 +112,23 @@ lspconfig.html.setup({ on_attach = on_attach })
 lspconfig.nextls.setup({ on_attach = on_attach })
 lspconfig.ts_ls.setup({ on_attach = on_attach })
 
+-- Autocommand for organizing imports and formatting Go files on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.go",
+    callback = function()
+        local params = vim.lsp.util.make_range_params()
+        params.context = { only = { "source.organizeImports" } }
+        -- Adjust timeout as needed (default: 1000ms)
+        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+        for cid, res in pairs(result or {}) do
+            for _, r in pairs(res.result or {}) do
+                if r.edit then
+                    local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+                    vim.lsp.util.apply_workspace_edit(r.edit, enc)
+                end
+            end
+        end
+        -- Format the file after organizing imports
+        vim.lsp.buf.format({ async = false })
+    end,
+})
--- lua/options.lua
+++ lua/options.lua
@@ -5,8 +5,8 @@ vim.opt.mouse = 'a'                 -- allow the mouse to be used in Nvim
 
 -- Tab
 vim.opt.tabstop = 2                 -- number of visual spaces per TAB
-vim.opt.softtabstop = 4             -- number of spacesin tab when editing
-vim.opt.shiftwidth = 4              -- insert 4 spaces on a tab
+vim.opt.softtabstop = 2             -- number of spacesin tab when editing
+vim.opt.shiftwidth = 2              -- insert 4 spaces on a tab
 vim.opt.expandtab = true            -- tabs are spaces, mainly because of python
 
 -- UI config
